5188c613ae548cea72e4268bfa9f63abe8b1b848
Jose M. Gomez <gomezjosem@live.com>
2024-05-22 03:48:45 +0000

    add getUserById query

--- query.js
+++ query.js
@@ -19,7 +19,16 @@ const getUsers = (request, response) => {
   })
 }
 // GET: /users/:id | getUserById()
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
 
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 // POST: /users | createUser()
 
 // PUT: /users/:id | updateUser()
