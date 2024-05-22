ea7623174c1776ef7ddfbde324c136f66eee9734
Jose M. Gomez <gomezjosem@live.com>
2024-05-22 03:50:58 +0000

    add deleteUser query

--- query.js
+++ query.js
@@ -57,3 +57,13 @@ const updateUser = (request, response) => {
   )
 }
 // DELETE: /users/:id | deleteUser()
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
