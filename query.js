4d98a14fcd0aa783d9852b85ea8adcc64ff9a08f
Jose M. Gomez <gomezjosem@live.com>
2024-05-22 03:50:34 +0000

    add updateUser query

--- query.js
+++ query.js
@@ -41,5 +41,19 @@ const createUser = (request, response) => {
   })
 }
 // PUT: /users/:id | updateUser()
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
 
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
 // DELETE: /users/:id | deleteUser()
