50d91dca65a9db4dff2ffea7cb2cff35477d0da8
Jose M. Gomez <gomezjosem@live.com>
2024-05-22 04:29:51 +0000

    add cors middleware

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+var cors = require('cors')
 const bodyParser = require('body-parser')
 const db = require('./queries')
 const app = express()
 const port = 3000
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
