4fed5840938612e61a9308a95079ffe1c1a71d9c
Jose M. Gomez <gomezjosem@live.com>
2024-05-22 03:48:19 +0000

    add getUsers query

--- query.js
+++ query.js
@@ -10,7 +10,14 @@ const pool = new Pool({
 })
 
 // GET: /users | getUsers()
-
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 // GET: /users/:id | getUserById()
 
 // POST: /users | createUser()
